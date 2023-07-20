8b95891b3d4f6ca227ccd669fa8707d6a334f636
wimdavies <william.johnston.davies@gmail.com>
2023-07-20 15:58:12 +0100

    Extract Square and Board into discrete component files imported into App.js

---
+++ src/components/Board.js
@@ -0,0 +1,66 @@
+import Square from "./Square";
+
+export default function Board({ xIsNext, squares, onPlay }) {
+  function handleClick(index) {
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
+
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[index] = "X";
+    } else {
+      nextSquares[index] = "O";
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+  const board = [];
+  const boardSize = 3;
+  for (let i = 0; i < boardSize; i++) {
+    const rows = [];
+
+    for (let j = 0; j < boardSize; j++) {
+      const index = i * boardSize + j;
+      rows.push(<Square key={index} value={squares[index]} onSquareClick={() => handleClick(index)} />);
+    }
+
+    board.push(<div key={i} className="board-row">{rows}</div>)
+  }
+
+  return (
+    <>
+      <div className="status">{status}</div>
+      {board}
+    </>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
